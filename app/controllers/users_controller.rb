74cfb1011f71da8915faa024833528b71457bb35
Anthony Mitchell <awmitchell@gmail.com>
2012-05-10 16:54:59 -0400

    minor devise-ish updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
-#class UsersController < ApplicationController
 class UsersController < Devise::RegistrationsController
   def new
     @user = User.new
     render :template => "users/registrations/new"
   end
   
-  # def create
-    # @user = User.new(params[:user])
-    # if @user.save
-      # redirect_to root_url, :notice => "Signed up!"
-    # else
-      # render "new"
-    # end
-  # end
-#   
+  def create
+    @user = User.new(params[:user])
+    
+    if @user.save
+      if @user.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(:user, @user)
+        respond_with @user, :location => after_sign_up_path_for(@user)
+      else
+        set_flash_message :notice, :"signed_up_but#{@user.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with @user, :location => after_inactive_sign_up_path_for(@user)
+      end
+    else
+      clean_up_passwords @user
+      render :template => "users/registrations/new"
+    end
+  end
+  
   # def show
     # @user = User.find(params[:id])
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 AspergersDating::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users"}
+  
+  devise_scope :user do
+    get "users/:id", :to => "profiles#show"
+  end
 
   root :to => "home#index"
 end
