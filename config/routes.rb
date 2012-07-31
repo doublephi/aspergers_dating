f100c766d191c3cf03f689edb5cfff223a67c4f2
Anthony Mitchell <awmitchell@gmail.com>
2012-07-31 14:55:30 -0400

    search results stylized

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ AspergersDating::Application.routes.draw do
   
   devise_scope :user do
     get "users/browse", :to => "profiles#browse", :as => :browse
-    get "users/:id", :to => "profiles#show"
+    get "users/:id", :to => "profiles#show", :as => :user
   end
 
   root :to => "home#index"
